b194545bf7074637a8cb6944651d0f65d671f8ff
Rahul Solanki <solanki.rahul45@gmail.com>
2026-04-16 00:47:18 +0530

    added the shop layout with auth

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import crownLogo from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <img src={crownLogo} className="logo" />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/auth'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
