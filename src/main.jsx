8cc3719819f9f2b162d7c6bdbcf0299702f3e2f5
Rahul Solanki <solanki.rahul45@gmail.com>
2026-04-18 02:24:22 +0530

    react context for state management

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './contexts/user.contexts.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </StrictMode>
   </BrowserRouter>
   
