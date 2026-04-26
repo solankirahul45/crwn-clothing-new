422c88ba6e99142d78c15aac110c9c414a35ca14
Rahul Solanki <solanki.rahul45@gmail.com>
2026-04-27 02:05:57 +0530

    adding cart context and shopping icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,7 +15,6 @@ const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
 
-
   return (
     <Fragment>
       <div className='navigation'>
