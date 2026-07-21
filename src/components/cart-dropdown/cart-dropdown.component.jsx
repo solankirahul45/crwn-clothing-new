d4625bfda8898618c3f8efef960c1cc51d9164c7
Rahul Solanki <solanki.rahul45@gmail.com>
2026-07-22 02:20:27 +0530

    user and cart reducer implementation

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
@@ -7,7 +9,6 @@ import CartItem from '../cart-item/cart-item.component';
 import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles.js';
 
 import './cart-dropdown.styles.js';
-import { useNavigate } from 'react-router-dom';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
