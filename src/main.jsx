630a39c77c14cd055827ead043e1990426b00143
Rahul <rahul@rahsol1mn1.local>
2026-07-23 01:37:50 +0530

    added redux store

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/user.context.jsx'
 import { CategoriesProvider } from './contexts/categories.context.jsx'
 import { CartProvider } from './contexts/cart.context.jsx'
+import { store } from './store/store.js';
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StrictMode>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </StrictMode>
-  </BrowserRouter>
-  
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
 )
