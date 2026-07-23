73dcb8097e1358623dbb7f72119840595638d1a4
Rahul Solanki <solanki.rahul45@gmail.com>
2026-07-24 04:01:30 +0530

    added category redux component

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { CategoriesProvider } from './contexts/categories.context.jsx'
 import { CartProvider } from './contexts/cart.context.jsx'
 import { store } from './store/store.js';
-import { Provider } from 'react-redux'
+
+import './index.scss'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
       <StrictMode>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </StrictMode>
     </BrowserRouter>
   </Provider>
