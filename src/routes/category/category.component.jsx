3f45d38644137bf875abaebee3b8baa03bac6cfb
Rahul <rahul@rahsol1mn1.local>
2026-07-24 20:01:15 +0530

    refactor the reducer based on the bussiness logic in our selectors

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesMap } from '../../store/categories/categories.selector';
 
 import './category.styles.scss';
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -57,13 +57,7 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapShot = await getDocs(q);
-  const categoryMap = querySnapShot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
-
-  return categoryMap;
+  return querySnapShot.docs.map((docSnapShot) => docSnapShot.data());
 };
 
 export const createUserDocumentFromAuth = async (
